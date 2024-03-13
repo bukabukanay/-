f85027337d45f923de3b24f8f4aac62073e5f630
machine <pavel228281228@gmail.com>
2024-03-14 00:22:39 +0500

    edit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,31 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+document.addEventListener('DOMContentLoaded', function() {
+  const tabsBtn = document.querySelectorAll('.tabButton');
+  const tabsContent = document.querySelectorAll('.cards');
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    });
-}
+  tabsBtn.forEach(onTabClick)
+
+  function onTabClick(item) {
+    item.addEventListener('click', function() {
+      let currentBtn = item;
+      let tabData = currentBtn.getAttribute('data-path');
+      let currentTabText = document.querySelector(`[data-target-text="${tabData}"]`);
+     /* let currentTabImg = document.querySelector(`[data-target-img="${tabData}"]`);*/
+
+      if (!currentBtn.classList.contains('tabs__btn-active')) {
+        tabsBtn.forEach(function(item) {
+          item.classList.remove('tabs__btn-active');
+        })
+
+        tabsContent.forEach(function(item) {
+          item.classList.remove('tabs-active')
+        })
+
+        currentBtn.classList.add('tabs__btn-active');
+        currentTabText.classList.add('tabs-active');
+       /* currentTabImg.classList.add('tabs-active');*/
+      }
+    })
+  }
+
+  /*document.querySelector('.js-tabBtn').click();*/
+})
