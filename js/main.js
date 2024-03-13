44ede89573453401d75ed409a79a36e37a620cd0
machine <pavel228281228@gmail.com>
2024-03-13 22:43:44 +0500

    edit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const cards = document.querySelectorAll('.card');
+const btnLeft = document.querySelector('.btn-left');
+const btnRight = document.querySelector('.btn-right');
+
+let currentIndex = 0;
+
+btnRight.addEventListener('click', () => {
+  if (currentIndex < cards.length - 3) {
+    currentIndex++;
+    cards[currentIndex].classList.add('card-hidden');
+    cards[currentIndex + 3].classList.remove('card-hidden');
+  }
+});
+
+btnLeft.addEventListener('click', () => {
+  if (currentIndex > 0) {
+    cards[currentIndex + 3].classList.add('card-hidden');
+    currentIndex--;
+    cards[currentIndex].classList.remove('card-hidden');
+  }
+});
